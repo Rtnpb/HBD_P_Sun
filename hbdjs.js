8b1d10830ee27f1f4c39308569a94134e21687d1
Rtnpb <65111521@dpu.ac.th>
2025-04-01 18:16:52 +0700

    น่าจะได้

--- hbdjs.js
+++ hbdjs.js
@@ -7,13 +7,13 @@ document.getElementById('birthday-message').addEventListener('click', function()
     
     let currentImageIndex = 0;
     const images = [
-        '/images/pSun1.jpg', '/images/pSun2.jpg', '/images/pSun3.jpg', '/images/pSun4.jpg', '/images/pSun5.jpg', '/images/pSun6.jpg',
-        '/images/pSun7.jpg', '/images/pSun8.jpg', '/images/pSun9.jpg', '/images/pSun10.jpg', '/images/pSun11.jpg', '/images/pSun12.jpg', 
-        '/images/pSun14.jpg', '/images/pSun15.jpg', '/images/pSun26.jpg', '/images/pSun18.jpg', '/images/pSun20.jpg', '/images/pSun21.jpg', '/images/pSun22.jpg',
-        '/images/pSun23.jpg', '/images/pSun24.jpg', '/images/pSun25.jpg', '/images/pSun17.jpg', '/images/pSun39.jpg',
-        '/images/pSun27.jpg', '/images/pSun28.jpg', '/images/pSun29.jpg', '/images/pSun30.jpg', '/images/pSun40.jpg',
-        '/images/pSun31.jpg', '/images/pSun32.jpg', '/images/pSun33.jpg', '/images/pSun34.jpg',
-        '/images/pSun35.jpg', '/images/pSun36.jpg', '/images/pSun37.jpg', '/images/pSun38.jpg'
+        'images/pSun1.jpg', 'images/pSun2.jpg', 'images/pSun3.jpg', 'images/pSun4.jpg', 'images/pSun5.jpg', 'images/pSun6.jpg',
+        'images/pSun7.jpg', 'images/pSun8.jpg', 'images/pSun9.jpg', 'images/pSun10.jpg', 'images/pSun11.jpg', 'images/pSun12.jpg', 
+        'images/pSun14.jpg', 'images/pSun15.jpg', 'images/pSun26.jpg', 'images/pSun18.jpg', 'images/pSun20.jpg', 'images/pSun21.jpg', 'images/pSun22.jpg',
+        'images/pSun23.jpg', 'images/pSun24.jpg', 'images/pSun25.jpg', 'images/pSun17.jpg', 'images/pSun39.jpg',
+        'images/pSun27.jpg', 'images/pSun28.jpg', 'images/pSun29.jpg', 'images/pSun30.jpg', 'images/pSun40.jpg',
+        'images/pSun31.jpg', 'images/pSun32.jpg', 'images/pSun33.jpg', 'images/pSun34.jpg',
+        'images/pSun35.jpg', 'images/pSun36.jpg', 'images/pSun37.jpg', 'images/pSun38.jpg'
     ];
 
     // แสดงรูปแรก
